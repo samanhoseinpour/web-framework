484ed13c61bb55c4b9e651b36483e9a1cafdc8eb
Saman <samangithoseinpour@gmail.com>
2023-09-04 15:57:48 +0330

    feat: triggering event callbacks

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,18 @@ import { User } from './models/User';
 
 const user = new User({ name: 'saman', age: 21 });
 
-user.on('onClick', () => {});
+user.on('onClick', () => {
+  console.log('User clicked.');
+});
 
-console.log(user);
+user.on('onSave', () => {
+  console.log('User saved something.');
+});
+
+user.on('onChange', () => {
+  console.log('User changed something.');
+});
+
+user.trigger('onClick');
+user.trigger('onSave');
+user.trigger('onChange');
