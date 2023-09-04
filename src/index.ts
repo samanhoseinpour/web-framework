6897cf07fb99875afc2d945cf4a573fdd8a1a855
Saman <samangithoseinpour@gmail.com>
2023-09-04 15:35:48 +0330

    feat: register event handlers for specific events

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { User } from './models/User';
 
 const user = new User({ name: 'saman', age: 21 });
 
-user.set({ name: 'sajjad', age: 25 });
+user.on('onClick', () => {});
 
-console.log(user.get('name'), user.get('age'));
+console.log(user);
