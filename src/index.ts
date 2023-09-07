1359a0d0decb3524b0a4aee6ecb84c5383f73b53
Saman <samangithoseinpour@gmail.com>
2023-09-07 11:12:02 +0330

    ref: refactor Eventing system with composition

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'saman hoseinpour', age: 21 });
+const user = new User({ id: 1 });
 
 user.save();
+
+user.events.on('change', () => {
+  console.log('change!');
+});
+user.events.trigger('change');
+
+user.events.on('click', () => {
+  console.log('clicked!');
+});
+user.events.trigger('click');
