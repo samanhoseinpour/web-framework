6066457017d69fd7289caceb857b1134133ea55d
Saman <samangithoseinpour@gmail.com>
2023-09-16 11:28:25 +0330

    ref: user props

--- src/models/User.ts
+++ src/models/User.ts
@@ -13,14 +13,4 @@ export class User {
   // Composition with Nested Objects
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync(rootUrl);
-
-  constructor(private data: UserProps) {}
-
-  get(userProp: string): string | number {
-    return this.data[userProp];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
 }
