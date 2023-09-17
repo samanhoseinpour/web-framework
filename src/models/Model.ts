2e4948bf47431d279cd9888b3f84d832a504eab8
Saman <samangithoseinpour@gmail.com>
2023-09-17 12:33:10 +0330

    ref: getter methods

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -27,17 +27,9 @@ export class Model<T extends HasId> {
     private events: Events
   ) {}
 
-  get on() {
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
+  on = this.events.on;
+  trigger = this.events.trigger;
+  get = this.attributes.get;
 
   set(update: T): void {
     this.attributes.set(update);
