2beb23f0f2cc13ebfec44838911366a2bac13755
Saman <samangithoseinpour@gmail.com>
2023-09-17 10:00:33 +0330

    feat: change user info while triggering

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import { User } from './models/User';
 
 const user = new User({ name: 'saman', age: 22 });
 
-console.log(user.get('name'));
-
 user.on('change', () => {
   console.log('user was changed.');
 });
 
-user.trigger('change');
+user.set({ name: 'sajjad' });
+
+console.log(user.get('name'));
