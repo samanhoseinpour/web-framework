b89dfcc776c42bbb901976836db7f6d860ee782a
Saman <samangithoseinpour@gmail.com>
2023-09-17 10:31:04 +0330

    ref: fetching user data

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'saman', age: 22 });
+const user = new User({ id: 1 });
 
 user.on('change', () => {
-  console.log('user was changed.');
+  console.log(user);
 });
 
-user.set({ name: 'sajjad' });
-
-console.log(user.get('name'));
+user.fetch();
