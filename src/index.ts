867f07ddc47a0e14f2b915c4f3f5513c22fa02df
Saman <samangithoseinpour@gmail.com>
2023-09-17 09:37:56 +0330

    fix: context issue around get methods

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'saman', age: 21 });
+const user = new User({ name: 'saman', age: 22 });
+
+console.log(user.get('name'));
 
 user.on('change', () => {
   console.log('user was changed.');
