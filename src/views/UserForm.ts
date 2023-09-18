e1efe62097460b1ac23352cfeb1e40ae36284ed9
Saman <samangithoseinpour@gmail.com>
2023-09-18 12:44:47 +0330

    feat: read user input and update the name state

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -14,11 +14,20 @@ export class UserForm {
   eventMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
+      'click:.update-name': this.updateName,
     };
   }
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
+  };
+
+  updateName = (): void => {
+    const input = this.parent.querySelector('input');
+
+    const name = input?.value;
+
+    this.model.set({ name });
   };
 
   template(): string {
@@ -27,8 +36,8 @@ export class UserForm {
             <h1>User Form</h1>
             <div>user name: ${this.model.get('name')}</div>
             <div>user age: ${this.model.get('age')}</div>
-            <input />   
-            <button>Click Me!</button> 
+            <input placeholder='change name'/>   
+            <button class="update-name">Update</button> 
             <button class="set-age">Set Random Age</button> 
         </div>
     `;
