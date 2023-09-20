a0c1c82d6c8955e05b8fd4820047f616aededfe0
Saman <samangithoseinpour@gmail.com>
2023-09-20 12:07:59 +0330

    feat: child view nesting

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -28,15 +28,10 @@ export class UserForm extends View<User, UserProps> {
 
   template(): string {
     return `
-        <div>
-            <h1>User Form</h1>
-            <div>user name: ${this.model.get('name')}</div>
-            <div>user age: ${this.model.get('age')}</div>
-            <input placeholder='change name'/>   
-            <button class="update-name">Update</button> 
-            <button class="set-age">Set Random Age</button> 
-            <button class="save-model">Save</button> 
-        </div>
+      <input placeholder='${this.model.get('name')}'/>   
+      <button class="update-name">Update</button> 
+      <button class="set-age">Set Random Age</button> 
+      <button class="save-model">Save</button>
     `;
   }
 }
